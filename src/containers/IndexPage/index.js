149657ba34577c4f6ac7e41712d3c0c53b3c0026
LukeYuan <d28827103@gmail.com>
2019-08-19 21:15:52 +0800

    my name card

--- src/containers/IndexPage/index.js
+++ src/containers/IndexPage/index.js
@@ -1,11 +1,53 @@
 import Link from 'next/link';
 import styled from 'styled-components';
 
+const EkkImage = styled.img``;
+
 const Card = styled.div`
-  width: 480px;
-  height: 640px;
-  border: solid 1px black;
+  position: relative;
+  width: 400px;
+  height: 600px;
+  border-radius: 30px;
+  background-color: hsl(215, 62%, 21%);
+  overflow: hidden;
+  
+  ${EkkImage} {
+    position: absolute;
+    bottom: 30px;
+    width:100%;
+  }
 `;
+
+
+const Logo = styled.img`
+  margin-top: 100px;
+  margin-left: 10px;
+  margin-right: 10px;
+`;
+
+
+const ChurchText = styled.p`
+  margin-top: 10px;
+  font-size: 50px;
+  text-align: center;
+  color: white;
+`;
+
+
+const NameText = styled.p`
+  margin-top: 20px;
+  font-size: 40px;
+  text-align: center;
+  letter-spacing: 10px;
+  text-indent: 10px;
+  color: white;
+  font-weight: 300;
+`;
+
+
+
+
+
 
 const A = styled.a``;
 
@@ -23,12 +65,15 @@ const Wrapper = styled.div`
   }
 `;
 
+
 const IndexPage = () => (
   <Wrapper>
-    <Card />
-    <Link href='/about' passHref>
-      <A>About Page</A>
-    </Link>
+    <Card>
+      <Logo src='/static/logo.svg' />
+      <ChurchText>新生命小組教會</ChurchText>
+      <NameText>王証頡</NameText>
+      <EkkImage src='/static/ekk.svg' />
+    </Card>
   </Wrapper>
 );
 
